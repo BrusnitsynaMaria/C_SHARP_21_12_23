617f02757648af22f9dea70fd89e96310ce5ade2
[Mary] <[brusnitsyna.maria@gmail.com]>
2024-03-04 16:50:16 +0500

    Add task 1.1

---
+++ lesson1/1.1/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.Write("Введите первое число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int b = Convert.ToInt32(Console.ReadLine());
+
+// String a1 = Console.ReadLine()!;
+// String b1 = Console.ReadLine()!;
+// int a = Convert.ToInt32(a1);
+// int b = Convert.ToInt32(b1);
+
+if(a == b * b) {
+    Console.WriteLine("Yes");
+} else {
+    Console.WriteLine("No");
+}
