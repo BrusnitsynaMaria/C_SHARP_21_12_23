9f1ba24aa6798e41743ca20f4bfbea271a35ca35
[Mary] <[brusnitsyna.maria@gmail.com]>
2024-03-10 20:01:13 +0500

    add task3

---
+++ lesson2/task3/Program.cs
@@ -0,0 +1,10 @@
+﻿//int n = 10;
+int[] array = [5, 6, 9, 12, 45, 5, 8, 7, 6, 3];
+int i = 0;
+
+while(i < array.Length) {
+    if(array[i] % 2 == 0) {
+        Console.Write($"{array[i]} ");
+    }
+    i++;
+}
