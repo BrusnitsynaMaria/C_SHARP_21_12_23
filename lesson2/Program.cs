40a2c83250cb7aa0f32ff4e8d245d4dc11b71076
[Mary] <[brusnitsyna.maria@gmail.com]>
2024-03-10 16:58:24 +0500

    Add girst task

---
+++ lesson2/Program.cs
@@ -0,0 +1,14 @@
+﻿int[] arr = new int [5];
+arr[0] = 5;
+arr[1] = 10;
+arr[2] = 0;
+arr[3] = 7;
+arr[4] = 1;
+Console.WriteLine(arr[0]);
+Console.WriteLine(arr[1]);
+Console.WriteLine(arr[2]);
+Console.WriteLine(arr[3]);
+Console.WriteLine(arr[4]);
+
+int[] arr2 = new int[5]{1, 3, 9, 78, 21};
+int[] arr3 = {1, 5, 9, 12, 78};
