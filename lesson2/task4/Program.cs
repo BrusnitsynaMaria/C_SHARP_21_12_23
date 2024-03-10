18cf02989b9a3ec0bc90e12ae9e6f296e7eb38ea
[Mary] <[brusnitsyna.maria@gmail.com]>
2024-03-10 20:36:37 +0500

    add task4

---
+++ lesson2/task4/Program.cs
@@ -0,0 +1,13 @@
+﻿int n = 5;
+int[] array = {2, 8, 9, 7, 6};
+int i = 0;
+int max = array[0];
+
+while(i < array.Length) {
+    if(array[i] > max) {
+        max = array[i];
+    }
+    i++;
+    }
+Console.WriteLine(max);
+
